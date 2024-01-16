7ea0b7d45f7cf4050d96268b9042c57b453f79e9
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-01-16 20:52:17 +0530

    e

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,32 @@
-<div class="max-h-screen max-w-screen flex flex-col justify-center items-center bg-[url('/bg.jpg')]">
-    <h1 class="text-4xl font-bold">Page</h1>
+<script lang="ts">
+    
+</script>
+
+<svelte:head>
+    <title>poster ella baralla</title>
+</svelte:head>
+
+<div class="text-white min-h-screen flex flex-col justify-center items-center bg-[url('/bg.jpg')]" style="background-size: cover; font-family: Avenir;">
+    <h1 class="text-6xl font-bold">dħRμVa</h1>
+    <h2 class="text-3xl">The astrophysics club of RVCE</h2>
+
+    <p class="md:w-[70%] p-10 pb-2">
+        Many have wondered where to study the science of astronomy and yearned for a dedicated club for the same! Well, we at dhRuVa aim for that very subject in our college, RVCE.
+    </p>
+
+    <p class="md:w-[70%] p-10 pt-0">
+        This club aims to bring about a change in the standard discourse on astronomy by making the topic more interesting by providing a hands-on approach to the many exciting challenges faced by astronomy and astrophysics.
+    </p>
+
+    <div class="flex">
+        <!-- LinkedIn Icon with Link -->
+        <a href="https://www.linkedin.com/company/dhruva-astronomy/" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </a>
+      
+        <!-- Instagram Icon with Link -->
+        <a href="https://www.instagram.com/dhruva_rvce/" target="_blank" rel="noopener noreferrer">
+          Instagram
+        </a>
+    </div>
 </div>
